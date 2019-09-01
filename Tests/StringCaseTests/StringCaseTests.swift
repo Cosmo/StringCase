567d633b6cc793e8b10bef1b6da134f87c503844
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-09-02 01:20:23 +0200

    Update StringCaseTests.swift

--- Tests/StringCaseTests/StringCaseTests.swift
+++ Tests/StringCaseTests/StringCaseTests.swift
@@ -2,14 +2,53 @@ import XCTest
 @testable import StringCase
 
 final class StringCaseTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(StringCase().text, "Hello, World!")
+    func testSnakeCase() throws {
+        XCTAssertTrue("snake".isSnakeCase)
+        XCTAssertTrue("snake_case".isSnakeCase)
+        XCTAssertTrue("snake_case_example".isSnakeCase)
+        XCTAssertFalse("not_a_SNAKECASE_String".isSnakeCase)
+        XCTAssertFalse("notSnakeCase".isSnakeCase)
+        XCTAssertFalse("AlsoNotSnakeCase".isSnakeCase)
+        
+        XCTAssertEqual("snake".snakeCased(), "snake")
+        XCTAssertEqual("snake cased".snakeCased(), "snake_cased")
+        XCTAssertEqual("snakeCased".snakeCased(), "snake_cased")
+        XCTAssertEqual("snake Cased_String".snakeCased(), "snake_cased_string")
+        XCTAssertEqual("_this is*  not-Very%difficult".snakeCased(), "this_is_not_very_difficult")
+    }
+    
+    func testLowerCamelCase() throws {
+        XCTAssertTrue("lower".isLowerCamelCase)
+        XCTAssertTrue("lowerCamelCase".isLowerCamelCase)
+        XCTAssertFalse("lowerCamelCase_with_underscore".isLowerCamelCase)
+        XCTAssertFalse("UpperCamelCase".isLowerCamelCase)
+        XCTAssertFalse("snake_case".isLowerCamelCase)
+        
+        XCTAssertEqual("lower".lowerCamelCased(), "lower")
+        XCTAssertEqual("LowerCamelCased".lowerCamelCased(), "lowerCamelCased")
+        XCTAssertEqual("lower_camel_cased".lowerCamelCased(), "lowerCamelCased")
+        XCTAssertEqual("Lower Camel cased".lowerCamelCased(), "lowerCamelCased")
+        XCTAssertEqual("_this is*  not-Very%difficult".lowerCamelCased(), "thisIsNotVeryDifficult")
+    }
+    
+    func testUpperCamelCase() throws {
+        XCTAssertTrue("Upper".isUpperCamelCase)
+        XCTAssertTrue("UpperCamelCase".isUpperCamelCase)
+        XCTAssertTrue("UpperCamelCaseExample".isUpperCamelCase)
+        XCTAssertFalse("UpperCamelCase_with_underscore".isUpperCamelCase)
+        XCTAssertFalse("snake_case".isUpperCamelCase)
+        XCTAssertFalse("lowerCamelCase".isUpperCamelCase)
+        
+        XCTAssertEqual("Upper".upperCamelCased(), "Upper")
+        XCTAssertEqual("upperCamelCased".upperCamelCased(), "UpperCamelCased")
+        XCTAssertEqual("upper_camel_cased".upperCamelCased(), "UpperCamelCased")
+        XCTAssertEqual("upper_camel Cased".upperCamelCased(), "UpperCamelCased")
+        XCTAssertEqual("_this is*  not-Very%difficult".upperCamelCased(), "ThisIsNotVeryDifficult")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSnakeCase", testSnakeCase),
+        ("testLowerCamelCase", testLowerCamelCase),
+        ("testUpperCamelCase", testUpperCamelCase),
     ]
 }
