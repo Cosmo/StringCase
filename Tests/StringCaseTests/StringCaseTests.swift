c7e868a479d575b057be85157baba0ef683e5166
Valentin Walter <valentinwalter@icloud.com>
2021-05-30 12:19:43 +0200

    Added kebab-case tests

--- Tests/StringCaseTests/StringCaseTests.swift
+++ Tests/StringCaseTests/StringCaseTests.swift
@@ -6,11 +6,13 @@ final class StringCaseTests: XCTestCase {
         XCTAssertTrue("snake".isSnakeCase)
         XCTAssertTrue("snake_case".isSnakeCase)
         XCTAssertTrue("snake_case_example".isSnakeCase)
+		XCTAssertFalse("SNAKE".isSnakeCase)
         XCTAssertFalse("not_a_SNAKECASE_String".isSnakeCase)
         XCTAssertFalse("notSnakeCase".isSnakeCase)
         XCTAssertFalse("AlsoNotSnakeCase".isSnakeCase)
         
         XCTAssertEqual("snake".snakeCased(), "snake")
+		XCTAssertEqual("SNAKE".snakeCased(), "snake")
         XCTAssertEqual("snake cased".snakeCased(), "snake_cased")
         XCTAssertEqual("snakeCased".snakeCased(), "snake_cased")
         XCTAssertEqual("snake Cased_String".snakeCased(), "snake_cased_string")
@@ -20,11 +22,13 @@ final class StringCaseTests: XCTestCase {
     func testLowerCamelCase() throws {
         XCTAssertTrue("lower".isLowerCamelCase)
         XCTAssertTrue("lowerCamelCase".isLowerCamelCase)
+		XCTAssertFalse("LOWER".isLowerCamelCase)
         XCTAssertFalse("lowerCamelCase_with_underscore".isLowerCamelCase)
         XCTAssertFalse("UpperCamelCase".isLowerCamelCase)
         XCTAssertFalse("snake_case".isLowerCamelCase)
         
         XCTAssertEqual("lower".lowerCamelCased(), "lower")
+		XCTAssertEqual("LOWER".lowerCamelCased(), "lower")
         XCTAssertEqual("LowerCamelCased".lowerCamelCased(), "lowerCamelCased")
         XCTAssertEqual("lower_camel_cased".lowerCamelCased(), "lowerCamelCased")
         XCTAssertEqual("Lower Camel cased".lowerCamelCased(), "lowerCamelCased")
@@ -35,20 +39,40 @@ final class StringCaseTests: XCTestCase {
         XCTAssertTrue("Upper".isUpperCamelCase)
         XCTAssertTrue("UpperCamelCase".isUpperCamelCase)
         XCTAssertTrue("UpperCamelCaseExample".isUpperCamelCase)
+		XCTAssertFalse("UPPER".isUpperCamelCase)
         XCTAssertFalse("UpperCamelCase_with_underscore".isUpperCamelCase)
         XCTAssertFalse("snake_case".isUpperCamelCase)
         XCTAssertFalse("lowerCamelCase".isUpperCamelCase)
         
         XCTAssertEqual("Upper".upperCamelCased(), "Upper")
+		XCTAssertEqual("UPPER".upperCamelCased(), "Upper")
         XCTAssertEqual("upperCamelCased".upperCamelCased(), "UpperCamelCased")
         XCTAssertEqual("upper_camel_cased".upperCamelCased(), "UpperCamelCased")
         XCTAssertEqual("upper_camel Cased".upperCamelCased(), "UpperCamelCased")
         XCTAssertEqual("_this is*  not-Very%difficult".upperCamelCased(), "ThisIsNotVeryDifficult")
     }
+	
+	func testKebabCase() throws {
+		XCTAssertTrue("kebab".isKebabCase)
+		XCTAssertTrue("kebab-case".isKebabCase)
+		XCTAssertTrue("kebab-case-example".isKebabCase)
+		XCTAssertFalse("KEBAB".isKebabCase)
+		XCTAssertFalse("not-a-KEBABCASE-String".isKebabCase)
+		XCTAssertFalse("notKebabCase".isKebabCase)
+		XCTAssertFalse("AlsoNotKebabCase".isKebabCase)
+		
+		XCTAssertEqual("kebab".kebabCased(), "kebab")
+		XCTAssertEqual("KEBAB".kebabCased(), "kebab")
+		XCTAssertEqual("kebab cased".kebabCased(), "kebab-cased")
+		XCTAssertEqual("kebabCased".kebabCased(), "kebab-cased")
+		XCTAssertEqual("kebab Cased_String".kebabCased(), "kebab-cased-string")
+		XCTAssertEqual("_this is*  not-Very%difficult".kebabCased(), "this-is-not-very-difficult")
+	}
 
     static var allTests = [
         ("testSnakeCase", testSnakeCase),
         ("testLowerCamelCase", testLowerCamelCase),
         ("testUpperCamelCase", testUpperCamelCase),
+		("testKebabCase", testKebabCase),
     ]
 }
